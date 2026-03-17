25dfbe9eeb37dcd381e26a73024d054a5f537fe3
vzapatag5 <vzapatag5@eafit.edu.co>
2026-03-17 15:11:40 -0500

    cambios dentro de yaml y script para que aparezca en artifacts

--- script.py
+++ script.py
@@ -1,20 +1,23 @@
 from playwright.sync_api import sync_playwright
+import os
 
 def run():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)  # para ver qué pasa
+        browser = p.chromium.launch(headless=True)
         page = browser.new_page()
 
-        # Página de prueba directa
         page.goto("https://raw.githubusercontent.com/cs109/2014_data/master/countries.csv")
 
-        # Guardar contenido directamente
         content = page.content()
 
-        with open("archivo_prueba.csv", "w", encoding="utf-8") as f:
+        # 👇 ruta absoluta segura
+        file_path = os.path.join(os.getcwd(), "archivo_prueba.csv")
+
+        with open(file_path, "w", encoding="utf-8") as f:
             f.write(content)
 
-        print("Archivo guardado correctamente")
+        print("Archivo guardado en:", file_path)
+
         browser.close()
 
 run()
